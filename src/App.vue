c0c5d0572ab4b1a3c1edb4449d76d4d889231f23
knight.gao <knight.gao@tineco.com>
2023-09-15 14:23:07 +0800

    feat: MVP版本完成

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <script setup lang="ts">
-import {useCanvas} from './hooks/useCanvas.ts';
-import {onMounted,onBeforeMount, onBeforeUnmount, onBeforeUpdate, onUnmounted, onUpdated} from "vue";
-
-const {a} = useCanvas();
-
-onBeforeMount(() => {
-  console.log("APPonBeforeMount")
-})
-
-onBeforeUpdate(() => {
-  console.log("APPonBeforeUpdate")
-})
-onUpdated(() => {
-  console.log("APPonUpdated")
-})
-onBeforeUnmount(() => {
-  console.log("APPonBeforeUnmount")
-})
-onUnmounted(() => {
-  console.log("APPonUnmounted")
-})
-
-
-onMounted(() => {
-  console.log("APPonMounted")
-})
-console.log(a);
+import MyCanvas from "./components/myCanvas.vue";
 </script>
 
 <template>
-  <div>22</div>
+  <div>
+    <my-canvas></my-canvas>
+  </div>
 </template>
 
 <style scoped>
